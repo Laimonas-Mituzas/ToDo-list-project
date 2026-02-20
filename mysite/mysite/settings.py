bccab6fdb3bf33fd69aee459b59df3a396216b8e
Laimonas <54275557+Laimonas-Mituzas@users.noreply.github.com>
2026-02-20 16:09:04 +0200

    Remove secret key and debug settings from configuration
    Removed sensitive settings for production security.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -19,15 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--&&-7x$$7^-ab*&=hrk69r(d8e$zluj@$8#vyti7g@p-o7q7)o'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
